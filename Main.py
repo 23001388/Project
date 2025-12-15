903db1b2ab55189dd62156c0812bdece4555da32
23001388 <23001388@gateshead.ac.uk>
2025-12-15 14:50:41 +0000

    Create function to add message
    Added random warning messages and improved expense calculation.

--- Main.py
+++ Main.py
@@ -1,4 +1,14 @@
-print("Welcome to your Budget Tracker!")
+import random
+warning_message = [
+  "You are doing well!You can save or invevest!",
+  "You should be careful with your spending",
+  "You are over spending and need to reduce"
+  ]
+def show_message(message):
+   return message[random.randint(0,(len(message)-1))]
+   
+
+
 
 #Budget =
 # Ask user for their income
@@ -11,6 +21,35 @@ food =float(input("please enter your food bill"))
 Bills =float(input("please enter your electricity bill"))
 Clothing =float(input("please enter your clothing bill"))
 
-Total_expenses = food + Bills + Clothing
-print(Total_expenses)
+# List of expenses
+expenses = [food, Bills, Clothing]
+
+# Loop to calculate total expenses
+Total_expenses = 0
+for item in expenses:
+  Total_expenses = Total_expenses + item
 #Remaining_income =
+
+# Output section
+print("Budget Report")
+print("Income: £", income)
+print("Food: £", food)
+print("Bills: £", Bills)
+print("Clothing: £", Clothing)
+print("Total expenses: £", Total_expenses)
+remaining_money =float(income) - float (Total_expenses)
+print("remaining_money: £", )
+print("suggested investment (20%): £", remaining_money)
+
+# If/elif/else advice section
+if remaining_money > 300:
+  print(f"{show_message(warning_message)}")
+elif remaining_money > 0:
+  print(f"You should be careful with your sprnding.")
+else:
+  print(f"You are overspending and need to reduce ")
+
+
+
+
+
