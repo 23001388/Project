ca20cec9aaf85109c5e92d1e1fede3282ba536b6
23001388 <23001388@gateshead.ac.uk>
2025-12-15 10:18:55 +0000

    Asking for input from the user
    Added basic structure for a Budget Tracker that prompts for income and expenses.

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
+print("Welcome to your Budget Tracker!")
 
+#Budget =
+# Ask user for their income
+income = input ("Enter your income: " )
+
+#Ask user for their expenses 
+print ("Enter your expenses:" )
+
+food =float(input("please enter your food bill"))
+Bills =float(input("please enter your electricity bill"))
+Clothing =float(input("please enter your clothing bill"))
+
+Total_expenses = food + Bills + Clothing
+print(Total_expenses)
+#Remaining_income =
